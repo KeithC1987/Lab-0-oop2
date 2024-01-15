4cfc5852b545df16d0d03bfdae72a047c71b5177
KeithC1987 <112564713+KeithC1987@users.noreply.github.com>
2024-01-14 22:26:36 -0700

    Add files via upload

---
+++ Lab 0 of OOP2/Lab 0 of OOP2/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace lab0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task one");
+
+            int lowNumber = GetPositiveNumber("Please enter the low number: ");
+
+            int highNumber = HighGreaterThanLow("Please enter the high number which is greather than low number : ", lowNumber);
+
+            int difference = highNumber - lowNumber;
+
+            Console.WriteLine($"The difference between {highNumber} and {lowNumber} is {difference}");
+        }
+        static int GetPositiveNumber(string prompt)
+        {
+            while (true)
+            {
+                Console.Write(prompt);
+                if (int.TryParse(Console.ReadLine(), out int number) && number > 0)
+                {
+
+                    return number;
+                }
+
+                else
+                {
+                    Console.WriteLine($"Please enter a positive number");
+                }
+
+            }
+
+        }
+        static int HighGreaterThanLow(string prompt, int low)
+        {
+            while (true)
+            {
+                Console.Write(prompt);
+                if (int.TryParse(Console.ReadLine(), out int number) && number > low)
+                {
+                    return number;
+                }
+                else
+                {
+                    Console.WriteLine($"Please enter a number greater than {low}.");
+                }
+            }
+
+
+
+        }
+
+
+
+    }
+
+    
+}
